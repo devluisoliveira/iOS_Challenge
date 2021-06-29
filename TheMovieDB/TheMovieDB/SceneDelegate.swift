ab3edf56c8e756974d99e1c4761d7d32b49ca113
Luis Oliveira <luis.oliveira@mobile2you.com.br>
2021-06-29 01:29:40 -0300

    UpdateSceneDelegate

--- TheMovieDB/TheMovieDB/SceneDelegate.swift
+++ TheMovieDB/TheMovieDB/SceneDelegate.swift
@@ -17,10 +17,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
-                
-                window = UIWindow(windowScene: scene)
-                window?.rootViewController = MainTabController()
-                window?.makeKeyAndVisible()
+    
+        let navController = UINavigationController()
+        coordinator = MainTabBarCoordinator(presenter: navController)
+        coordinator?.start()
+        navController.isNavigationBarHidden = true
+        
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
