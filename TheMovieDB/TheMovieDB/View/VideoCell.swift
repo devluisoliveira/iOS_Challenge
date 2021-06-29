204dadb1629a8beabcbf1a8b9bf23845b1f3c051
Luis Oliveira <luis.oliveira@mobile2you.com.br>
2021-06-29 01:08:03 -0300

    Create VideoCell and Pod Install YoutubePlayer

---
+++ TheMovieDB/TheMovieDB/View/VideoCell.swift
@@ -0,0 +1,37 @@
+//
+//  VideoCell.swift
+//  TheMovieDB
+//
+//  Created by DevLuis on 29/06/21.
+//
+
+import UIKit
+import YouTubePlayer
+
+class VideoCell: UICollectionViewCell {
+    //MARK: - Properties
+    var key: String? = ""
+    
+    lazy var videoPlayer: YouTubePlayerView = {
+        let frameForPlayer = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height))
+        let view = YouTubePlayerView(frame: frame)
+        
+        return view
+    }()
+
+    //MARK: - Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(videoPlayer)
+        videoPlayer.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, height: 260)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+            videoPlayer.loadVideoID("\(key?.description ?? "")")
+    }
+}
